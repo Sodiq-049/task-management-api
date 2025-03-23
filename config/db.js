8e583974e48e3cbf86fc23193eb0cdf173b580d5
Sodiq-049 <sodiqogunmola049@gmail.com>
2025-03-23 17:23:23 +0100

    update db config

--- config/db.js
+++ config/db.js
@@ -1,20 +1,18 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-// Load environment variables from .env file
-dotenv.config();
+dotenv.config(); // Load environment variables
 
 const connectDB = async () => {
     try {
-        // Connect to MongoDB using the connection string from the .env file
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true, // This option is deprecated but can be kept for now
-            useUnifiedTopology: true, // This option is also deprecated but can be kept for now
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         });
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection failed:', error);
-        process.exit(1); // Exit the process with failure
+        process.exit(1);
     }
 };
 
